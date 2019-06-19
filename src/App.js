ef99014d8598d7a1fd0f1a2c1ad10d163488f606
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-06-18 20:24:11 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+import React from 'react';
+import { createStore, applyMiddleware } from "redux"
+
+const initialState = {}
+
+const middleware = []
+
+const rootReducer = (store = initialState, action) => {
+return initialState
+}
+
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+console.log("store", store);
+
+
+function App(){
+  
+    return (
+      
+      <div className="app__container">
+
+        
+      </div>
+      
+    )
+  }
+
+export default App;
+
+//App.js
+  // fetch flight info
+    // search bar for flights
+    // display in order of eta
+    // create list from data
+    // cache flights
+  
+  //::::DropOff.js
+    //Display list of outgoing (need hotel api)
+    //Display 15s and 45s
+    //
+
+  //::::Flight.js::::
+    // airline name or logo (color coded)
+    // flight number
+    // status (color coded) - delayed+eta/canceled/eta/arrived
+    //Side to pick up(maybe)
+      //::::Methods:::::
+      // click to time stamp and add to completed list (need back-end)
+      //
+
+
---
+++ src/components/Arrivals/Arrivals.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Arrivals extends Component {
+    render(){
+        
+        return (
+            <div className="arrivals__container">
+                
+            </div>
+        )
+    }
+}
