4b9c1fb37e8afd847784795cfff01820849b42b1
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-06-29 13:40:42 -0400

    testing navbar 2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,24 +12,50 @@ export default class Navbar extends Component{
     render(){
     return (
         <div className="navbar__container">
-
-            <div className="arrivals__background"></div>
-            <div className="departures__background"></div>
+            <div className="navbar__img"></div>
+            {/* <div className="arrivals__background"></div>
+            <div className="departures__background"></div> */}
             
-            
-           
-                <div className="navbar__img"></div>
-                <Link to="/arrivals"><h1 className="navbar__arrivals__header">Arrivals</h1><div className="navbar__arrivals__container"></div>
-                </Link>
-                
+            <div className="navbar__arrivals__background"></div>
+            <div className="navbar__departures__background"></div>
+            <div className="navbar__picked-up__background"></div>
+    
+                <div className="navbar__arrivals__container">
+                    <Link to="/arrivals">
+                Arrivals</Link>
                 <div className="navbar__arrivals__corner"></div>
                 
-                <Link to="/departures"> <h1 className="navbar__departures__header">Departures</h1></Link>
+                </div>
+                
+                <div className="navbar__departures__container">
+                    <Link to="/departures">
+                Departures</Link>
+                <div className="navbar__departure__corner"></div>
+                </div>
+                
+                <div className="navbar__picked-up__container">
+                    <Link to="/pickedup">
+                Picked-Up</Link>
+                <div className="navbar__picked-up__corner"></div>
+                </div>
+                
+                <div className="navbar__valet__container">
+                    <Link to="/valet">
+                Valet</Link>
+                <div className="navbar__valet__corner"></div>
+                </div>
+                
+                
+                
+               
+                
+                {/* <Link to="/departures"> <h1 className="navbar__departures__header">Departures</h1></Link>
                 
                 <div className="navbar__departures__corner"></div>
+                
                 <Link to="/pick-up"><h1 className="navbar__picked-up__header">Picked-Up</h1></Link>
                 <div className="navbar__picked-up__corner"></div>
-                <Link to="/valet"><h1 className="navbar__valet__header">Valet</h1></Link>
+                <Link to="/valet"><h1 className="navbar__valet__header">Valet</h1></Link> */}
                 
               
         </div>
