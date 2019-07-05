0c80607f6b24ec727806cb4a67d95f9be790d159
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-07-04 21:11:31 -0400

    removed redux

--- src/App.js
+++ src/App.js
@@ -3,27 +3,9 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar"
 import Arrivals from "./components/Arrivals/Arrivals"
-import { createStore, applyMiddleware } from "redux"
 import Departures from './components/Departures/Departures';
 import Received from './components/Received/Received';
 import Valet from './components/Valet/Valet';
-
-
-
-const initialState = {}
-
-const middleware = []
-
-const rootReducer = (store = initialState, action) => {
-return initialState
-}
-
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
-console.log("store", store);
-
-
-
-
 
 
 function App(){
